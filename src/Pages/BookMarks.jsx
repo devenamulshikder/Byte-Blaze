2fd2b3c487954ee0a427dc68e4c509f60c3dd8fb
devenamulshikder <devenamulshikder@gmail.com>
2024-06-18 00:23:11 -0300

    byte-blaze complete

--- src/Pages/BookMarks.jsx
+++ src/Pages/BookMarks.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { deleteBlogs, getBlogs } from "../utils";
 import BlogCard from "../components/BlogCard";
+import EmptyState from "../components/EmptyState";
 
 const BookMarks = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,12 @@ const BookMarks = () => {
     deleteBlogs(id)
     const storedBlogs = getBlogs();
     setBlogs(storedBlogs);
+  }
+  if (blogs.length<1) {
+    return <EmptyState message='No Booksmarks Avilable! '
+    address='/blogs'
+    label='Browse Blogs'
+    />
   }
   return (
     <div className="grid justify-center px-4 sm:px-8 lg:px-12 py-8 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ import rehypeRaw from "rehype-raw";
 
 const Content = () => {
   const blog = useLoaderData();
-  const { cover_image, title, tags, body_html } = blog;
+  const { cover_image, title, tags, body_html, url } = blog;
   return (
     <div className="mx-auto group transition border-2 p-2 border-opacity-30 hover:no-underline focus:no-underline">
       <img
@@ -26,9 +26,9 @@ const Content = () => {
           ))}
         </div>
       <div className="space-y-2">
-        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+        <a href={url} target="_blank" className="text-2xl font-semibold group-hover:underline group-focus:underline">
           {title}
-        </h3>
+        </a>
         <Markdown rehypePlugins={rehypeRaw}>
             {body_html}
         </Markdown>
